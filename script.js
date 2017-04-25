bf42235d921ee70397f4e032d364cbae3a7505da
Aerelloth <oceane.scheidel@gmail.com>
2017-04-25 09:22:13 +0200

    MàJ linguistique + explication des fonctions

--- script.js
+++ script.js
@@ -4,13 +4,15 @@
 
 	/******************************[ Liste 1 ]******************************/
 
-	var nomAgent = ['le chat du voisin', 'ma grand-mère', 'Alexandre le Grand', 'Chuck Norris', 'Céline Dion', 'le pape', 'Frodon', 'ma petite soeur', 'le facteur', 'un kangourou', 'mon prof de maths', 'Superman', 'un bonhomme de neige', 'Barbie', 'le père Noël', 'le boss du dernier niveau', 'la mariée', 'le plombier', 'le roi', 'la reine', 'un perroquet déplumé', 'Napoléon', 'le roi Arthur', 'Merlin', 'un loup-garou', 'Dracula', 'un extra-terrestre', 'le prince charmant', 'une licorne', 'la petite souris', 'le lapin de Pâques', 'Madame la Duchesse', 'Astérix', 'Lucky Luke', 'le Grand Schtroumpf'];
+	var nomAnime = ['le chat du voisin', 'ma grand-mère', 'Alexandre le Grand', 'Chuck Norris', 'Céline Dion', 'le pape', 'Frodon', 'ma petite soeur', 'le facteur', 'un kangourou', 'mon prof de maths', 'Superman', 'un bonhomme de neige', 'Barbie', 'le père Noël', 'le boss du dernier niveau', 'la mariée', 'le plombier', 'le roi', 'la reine', 'un perroquet déplumé', 'Napoléon', 'le roi Arthur', 'Merlin', 'un loup-garou', 'Dracula', 'un extra-terrestre', 'le prince charmant', 'une licorne', 'la petite souris', 'le lapin de Pâques', 'Madame la Duchesse', 'Astérix', 'Lucky Luke', 'le Grand Schtroumpf'];
 
 	var verbe = ['a mangé', 'a toujours détesté', 'a vendu', 'adore', 'a tué', 'a battu aux échecs', 'a recueilli', 's\'acharne sur', 'apprécie', 'a bu', 'a provoqué en duel', 'a joué un sale tour à', 'a épousé', 'a voyagé avec', 'ne supporte pas', 'a adoubé', 'a embauché', 'a renvoyé', 'encourage', 'se désespère de rencontrer', 'voudrait partir en vacances avec', 'a complètement oublié', 'a téléchargé', 'a voté pour', 'est tout à fait contre', 'a fait un gros câlin à', 's\'est brossé les dents avec'];
 
 	var nom = ['une paire de gants pour rosiers', 'une passoire à nouilles', 'un pingouin en peluche', 'un esquimau', 'Venise', 'mon téléphone portable', 'un script JS', 'une cuillère en bois', 'des salsifis', 'les fruits au sirop', 'l\'Anneau Unique', 'la machine à remonter le temps', 'la Tour Eiffel', 'un crapaud', 'des sushis', 'la glace à la fraise', 'une lampe à huile', 'une tondeuse à gazon', 'du fromage', 'une râpe à fromage'];
-	//ajout des noms actifs à la liste des noms passifs
-	Array.prototype.push.apply(nom, nomAgent);
+	//ajout des noms animés à la liste des noms non-animés :
+	//Array.prototype.push() ajoute un/des élément(s) à la fin d'un tableau.
+	//apply() passe à la fonction des arguments sous forme de tableau
+	Array.prototype.push.apply(nom, nomAnime);
 
 	var complement = ['au petit-déjeuner', 'au 12ème siècle', 'le jour de son anniversaire', 'pour s\'amuser', 'parce que c\'était l\'heure', 'par amour', 'avec des frites', 'sur le tabouret de la cuisine', 'à Paris', 'sous la pluie', 'le premier jour des soldes', 'en pleine voie', 'pour 5 euros', 'pour sauver sa peau', 'sans s\'en rendre compte', 'avant de se marier', 'au cinéma', 'sur la banquise', 'dans le jardin', 'avec une brosse à dents', 'à la petite cuillère', 'avec une batte de base-ball', 'sur le pont d\'Avignon', 'à -50%', 'en pleine nuit', 'un soir de pleine lune', 'pendant son cours d\'aqua-poney'];
 	//ajout de compléments de type "avec + nom" à la liste de compléments
@@ -18,7 +20,7 @@
 		complement.push('avec ' + nom[i]);
 	};
 
-	var liste1 = [nomAgent, verbe, nom, complement];
+	var liste1 = [nomAnime, verbe, nom, complement];
 
 	/******************************[ Liste 2 ]******************************/
 
@@ -39,6 +41,7 @@
 	//génère un nombre entier aléatoire compris entre « min » et « max » inclus
 	function getRandomInt(min, max) {
 	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	   //Math.random() génère un nombre flottant aléatoire dans l'intervalle [0, 1[
 	}
 
 	//retourne un élément aléatoire d'une liste (tableau à 1 dimension)
